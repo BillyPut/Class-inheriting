c49300815c81f98ff418c143d61ef350ccf2dc8c
BillyPut <90899464+BillyPut@users.noreply.github.com>
2022-09-28 10:13:58 +0100

    Did the thing

---
+++ Assets/Scripts/DragonScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonScript : EnemyBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttackPlayer();
+    }
+
+    public override void AttackPlayer()
+    {
+        print("Dragon is attacking player");
+    }
+
+
+}
